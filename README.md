fb073b20276d120e372ae2954ae61f8d20bcf0f9
arthurlm44 <arthurlm44@gmail.com>
2016-07-27 23:53:30 -0700

    move install script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# soundbored
+# Soundbored
 Spice up your terminal
+
+# INSTALLATION
+Run the install script with `sh scripts/install.sh`
+
+Created by github.com/arthurlm44
---
+++ scripts/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Setting up the soundbored sounds directory at /usr/local/var/soundbored/sounds"
+mkdir -p /usr/local/var/soundbored/sounds
+echo "Bootstrapping the sounds directory"
+cp sounds/* /usr/local/var/soundbored/sounds/
+echo "Adding the default songs.yml to /usr/local/soundbored/sounds"
+cp songs.yml /usr/local/var/soundbored
+echo "Installing soundbored at /usr/local/bin/soundbored"
+cp soundbored /usr/local/bin/soundbored && chmod 755 /usr/local/bin/soundbored
